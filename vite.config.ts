bc4f8b2d8458e76ccb60152ed5362dd10ec684f8
waniazanib <188218162+waniazanib@users.noreply.github.com>
2026-06-22 20:38:23 +0500

    edited vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
+    // nitro/vite builds from this
     server: { entry: "server" },
   },
-  // Add this block to override the Lovable default preset and force a Vercel build
-  vite: {
-    nitro: {
-      preset: 'vercel'
-    }
-  }
 });
